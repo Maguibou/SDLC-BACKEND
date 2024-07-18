df3ed71fdad01367b02ccbd3f9817a4cb0c493f3
Maguibou <159534525+Maguibou@users.noreply.github.com>
2024-07-18 13:52:06 +0100

    ajout de table a la base de données

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email', 191)->unique();
+            $table->enum("type",['stranger','admin','accompanist']);
             $table->string('password');
             $table->string('photo')->nullable();
             $table->rememberToken();
